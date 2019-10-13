54081ef538f8fe1b9d81781ddc7d39f26547a2da
keshava kotri <keshav143420@gmail.com>
2019-10-13 21:19:48 +0530

    Added login logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,56 @@
 import React from 'react';
 
+import * as ROUTES from '../constants/routes';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    // const { email, password } = this.state;
+
+    this.props.history.push(ROUTES.LANDING);
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
+    const { email, password, error } = this.state;
+    const isInvalid = password === '' || email === '';
+
     return (
-      <div>Login</div>
+      <form onSubmit={this.onSubmit}>
+      <input
+        name="email"
+        value={email}
+        onChange={this.onChange}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="password"
+        value={password}
+        onChange={this.onChange}
+        type="password"
+        placeholder="Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign In
+      </button>
+
+      {error && <p>{error.message}</p>}
+    </form>
     );
   }
 }
