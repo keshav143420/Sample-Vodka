047b3b07eef348aa4ba0913205c578f587447393
keshava kotri <keshav143420@gmail.com>
2019-10-13 21:50:48 +0530

    Added error login to failed login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,10 @@ const INITIAL_STATE = {
   email: '',
   password: '',
   error: null,
+  failed: {
+    count: 0,
+    message: ''
+  }
 };
 
 class Login extends React.Component {
@@ -13,44 +17,62 @@ class Login extends React.Component {
     super(props);
 
     this.state = { ...INITIAL_STATE };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onSubmit = event => {
-    // const { email, password } = this.state;
-
-    this.props.history.push(ROUTES.LANDING);
     event.preventDefault();
+    const { email, password } = this.state;
+
+    if (email === 'admin' && password === 'admin') {
+      this.props.history.push(ROUTES.LANDING);
+    } else {
+      this.setState((state) => ({
+        error: { message: `email and password combinations are wrong` },
+        failed: { count: state.failed.count + 1}
+      }))
+      console.log(this.state);
+    }
+    if (this.state.failed.count >= 3){
+      this.setState((state) => ({
+        error: { message: `${this.state.failed.count} failed login attemp` }
+      }))
+    }
+
   };
 
   onChange = event => {
+    event.preventDefault();
     this.setState({ [event.target.name]: event.target.value });
   };
   render() {
-    const { email, password, error } = this.state;
+    const { email, password, error, failed } = this.state;
     const isInvalid = password === '' || email === '';
 
     return (
       <form onSubmit={this.onSubmit}>
-      <input
-        name="email"
-        value={email}
-        onChange={this.onChange}
-        type="text"
-        placeholder="Email Address"
-      />
-      <input
-        name="password"
-        value={password}
-        onChange={this.onChange}
-        type="password"
-        placeholder="Password"
-      />
-      <button disabled={isInvalid} type="submit">
-        Sign In
-      </button>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} ref="login" type="submit">
+          Sign In
+        </button>
 
-      {error && <p>{error.message}</p>}
-    </form>
+        {error && <p>{error.message}</p>}
+        {failed.count !== 0 && <p>{failed.message}</p>}
+      </form>
     );
   }
 }
